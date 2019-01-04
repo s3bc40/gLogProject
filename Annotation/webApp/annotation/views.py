2c92b552287620b301e63b81c4de6b46a03dc709
s3bc40 <leseb40@gmail.com>
2019-01-04 11:38:58 +0100

    Django tuto finished : utilities for project

--- Annotation/webApp/annotation/views.py
+++ Annotation/webApp/annotation/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
+from .models import Post
 #from django.http import HttpResponse
 
-posts = [
-    {
-        'author' : 'SGC',
-        'title' : 'First Post 1',
-        'content' : 'First post content',
-        'date_posted': '01/03/2019'
-    },
-    {
-        'author' : 'BZH',
-        'title' : 'First Post 2',
-        'content' : 'Second post content',
-        'date_posted': '01/02/2019'
-    }
-]
+# posts = [
+#     {
+#         'author' : 'SGC',
+#         'title' : 'First Post 1',
+#         'content' : 'First post content',
+#         'date_posted': '01/03/2019'
+#     },
+#     {
+#         'author' : 'BZH',
+#         'title' : 'First Post 2',
+#         'content' : 'Second post content',
+#         'date_posted': '01/02/2019'
+#     }
+# ]
+
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        #'posts': posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'annotation/home.htm', context)
 
