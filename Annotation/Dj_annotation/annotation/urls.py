9da843dc788e6d185062386c38677254f4ebec57
s3bc40 <leseb40@gmail.com>
2019-01-05 13:25:07 +0100

    Annotation page on : basic one need submit button and script python part

--- Annotation/Dj_annotation/annotation/urls.py
+++ Annotation/Dj_annotation/annotation/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='annot-home'),
+    path('annotation/',views.process, name='annot-process')
 ]
--- Annotation/Dj_annotation/annotation/views.py
+++ Annotation/Dj_annotation/annotation/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'annotation/home.html')
+
+def process(request):
+    return render(request,'annotation/process.html')
