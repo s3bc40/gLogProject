beeb2bd150a1bfe82c8ae79ac931b02a40e13415
s3bc40 <leseb40@gmail.com>
2019-01-14 16:20:35 +0100

    Blast locally : DONE. Need add README.md to install blast+

--- Annotation/main.py
+++ Annotation/main.py
@@ -14,27 +14,29 @@
 import Fasta as fasta
 
 from Bio import SeqIO
-from Bio.Blast import NCBIWWW
-
+#from Bio.Blast import NCBIWWW
+from Bio.Blast.Applications import NcbiblastxCommandline
 import time
 
 # --------------------------
 #       Main
 # --------------------------
 if __name__ == "__main__":
-    obj = fasta.multFasta()
-    obj.readFasta()
-    fastaRec = obj.fasta
+    #obj = fasta.multFasta()
+    #obj.readFasta()
+    #fastaRec = obj.fasta
 # Blastx : annot fonc  
     timer=time.time()
-    for seqRec in fastaRec:
-        print("# Lancement Blast #\n")
-        result_handle = NCBIWWW.qblast("blastx", "swissprot", seqRec.seq)
-        print("# Ecriture dans XML #\n")
-        blast_result = open("data/blastMult.xml", "a")
-        blast_result.write(result_handle.read())
-        blast_result.close()
-        result_handle.close()
+    #for seqRec in fastaRec:
+    print("# Lancement Blast #\n")
+    blastx_cline = NcbiblastxCommandline(query="data/single.fasta", db="chrom1MM",evalue=1e-20,  outfmt=5, out="my_blast.xml")
+    stdout, stderr = blastx_cline()
+    #result_handle = NCBIWWW.qblast("blastx", "swissprot", seqRec.seq)
+    #print("# Ecriture dans XML #\n")
+    # blast_result = open("data/blastMult.xml", "a")
+    # blast_result.write(result_handle.read())
+    # blast_result.close()
+    # result_handle.close()
     print(time.time()-timer, "s")
 
 # http://www.geneontology.org/page/go-annotation-file-formats
